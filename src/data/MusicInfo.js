b4d0283164c3a4df72e2f5d2fe55505812744435
NOR0i <sarah.ma@torontomu.ca>
2025-11-16 13:21:30 -0500

    Update MusicInfo.js

--- src/data/MusicInfo.js
+++ src/data/MusicInfo.js
@@ -7,6 +7,10 @@ const MusicInfo = [
         image: 'cova.png',
         link: 'https://open.spotify.com/album/49NBXQ9Uze9hNjbCBFVhJR?si=NkFqSceWTbewh4A4SVWMcQ'
     },
+    {
+        image: 'angelisk.png',
+        link: 'https://open.spotify.com/album/7cWEuGoZ4v3SPMUnH1z7Lv?si=PnqXoV1ERHKScRyfOd387w'
+    },
 
 ]
 export default MusicInfo
